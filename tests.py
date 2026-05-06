56022b67761e342d91ac926bff52b3b13220f599
Ferdinand.B <157275289+FerdinandBodmann@users.noreply.github.com>
2026-05-06 11:15:06 +0200

    1.2.2.2
    Added Progress-Checkers to the structure file.

--- tests.py
+++ tests.py
@@ -2,6 +2,16 @@ import os
 commands = "start/stop/exit"
 status = 0
 progress = 0
+pro1 = "|>---------|"
+pro2 = "|->--------|"
+pro3 = "|-->-------|"
+pro4 = "|--->------|"
+pro5 = "|---->-----|"
+pro6 = "|----->----|"
+pro7 = "|------>---|"
+pro8 = "|------->--|"
+pro9 = "|-------->-|"
+pro10 = "|--------->|"
 
 print(commands)
 command = input()
@@ -9,9 +19,12 @@ if command == "start":
     status = 1
     print("ja/nein", "losfahren?")
     drive = input()
+
     if drive == "ja":
         status = 1
         progress = 1
+        print(pro1)
+
     if drive == "nein":
         status = 0
 if command == "stop" and status == 1:
