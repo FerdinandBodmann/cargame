9fda0adfb1cc38dd7b271ccebf9912f6a4fa2e18
Ferdinand.B <157275289+FerdinandBodmann@users.noreply.github.com>
2026-05-06 11:32:59 +0200

    Update tests.py
    Added a Status/Progress logic.

--- tests.py
+++ tests.py
@@ -25,8 +25,53 @@ if command == "start":
         print(pro1)
         drive1 = input(continue_drive)
         if drive1 == "ja":
+            status = 1
+            progress = 2
+            print(pro2)
+            drive2 = input(continue_drive)
+            if drive2 == "ja":
+                status = 1
+                progress = 3
+                print(pro3)
+                drive3 = input(continue_drive)
+                if drive3 == "ja":
+                    status = 1
+                    progress = 4
+                    print(pro4)
+                    drive4 = input(continue_drive)
+                    if drive4 == "ja":
+                        status = 1
+                        progress = 5
+                        print(pro5)
+                        drive5 = input(continue_drive)
+                        if drive5 == "ja":
+                            status = 1
+                            progress = 6
+                            print(pro6)
+                            drive6 = input(continue_drive)
+                            if drive6 == "ja":
+                                status = 1
+                                progress = 7
+                                print(pro7)
+                                drive7 = input(continue_drive)
+                                if drive7 == "ja":
+                                    status = 1
+                                    progress = 8
+                                    print(pro8)
+                                    drive8 = input(continue_drive)
+                                    if drive8 == "ja":
+                                        status = 1
+                                        progress = 9
+                                        print(pro9)
+                                        drive9 = input(continue_drive)
+                                        if drive9 == "ja":
+                                            status = 1
+                                            progress = 10
+                                            print(pro10)
+                                            print("Du bist am Ziel.")
     if drive == "nein":
         status = 0
+        command = input(commands)
 if command == "stop" and status == 1:
     status = 0
 if command == "stop" and status == 0:
